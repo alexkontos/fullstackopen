e3d8d75aae454fcad99d4662a2d380c3f23c0894
Alex Kontos <alex.a.kontos@gmail.com>
2023-01-12 15:47:01 +0200

    Simplify part 2.11 react app

--- part2/2.11_phonebook/src/App.js
+++ part2/2.11_phonebook/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import PersonsList from './components/PersonsList';
+import PersonForm from './components/PersonForm';
+import Filter from './components/Filter';
+import Header from './components/Header';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setFilterValue] = useState('')
+  const [isFiltered, setIsFiltered] = useState(false)
+
+  const personsToShow = isFiltered
+    ? persons.filter(person => person.name.toUpperCase().includes(filterValue.toUpperCase()))
+    : persons
+
+  const handleNameInputChange = e => {
+    setNewName(e.target.value);
+  }
+
+  const handleNumberInputChange = e => {
+    setNewNumber(e.target.value);
+  }
+
+  const handleFilterInputChange = e => {
+    let value = e.target.value;
+    setFilterValue(value)
+    if (value) {
+      setIsFiltered(true)
+    }
+    else {
+      setIsFiltered(false)
+    }
+  }
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    if (!persons.some(person => person.name === newName)) {
+      setPersons([...persons, { name: newName, number: newNumber, id: persons.length + 1 }])
+    } else {
+      alert(`${newName} is already in the phonebook`)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header text="Phonebook" />
+      <Filter value={filterValue} onChange={handleFilterInputChange} />
+      <PersonForm newName={newName} handleNameInputChange={handleNameInputChange} newNumber={newNumber} handleNumberInputChange={handleNumberInputChange} handleFormSubmit={handleFormSubmit} />
+      <PersonsList personsToShow={personsToShow} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
